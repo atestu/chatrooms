8bc3aa9da6c8f554c3f0be8d3998320e1d19a20f
Alexandre Testu <atestu@gmail.com>
2011-09-08 01:10:16 -0400

    Show John & co

---
+++ server.js
@@ -0,0 +1,188 @@
+var connect = require('connect'),
+	express = require('express'),
+	sys = require('sys'),
+	io = require('socket.io'),
+	knox = require('knox'),
+	port = (process.env.PORT || 8081);
+
+// set global variable on the server to avoid a keys.js
+var knoxClient = knox.createClient({
+	key: process.env['RPSAWSID'],
+	secret: process.env['RPSAWSSECRET'],
+	bucket: "mturk-chatrooms"
+});
+
+//Setup Express
+var app = express.createServer();
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.use(connect.bodyDecoder());
+	app.use(connect.staticProvider(__dirname + '/static'));
+	app.use(app.router);
+});
+
+//setup the errors
+app.error(function(err, req, res, next){
+	if (err instanceof NotFound) {
+		res.render('404.ejs', {
+			locals: { 
+				header: '#Header#',
+				footer: '#Footer#',
+				title : '404 - Not Found',
+				description: '',
+				author: '',
+				analyticssiteid: 'XXXXXXX' 
+				},status: 404 });
+			}
+	else {
+		res.render('500.ejs', {
+			locals: { 
+				header: '#Header#',
+				footer: '#Footer#',
+				title : 'The Server Encountered an Error',
+				description: '',
+				author: '',
+				analyticssiteid: 'XXXXXXX',
+				error: err 
+				},status: 500 });
+			};
+});
+
+app.listen(port);
+
+
+function writeJSON (message, filename) {
+  var req = knoxClient.put(filename, {
+      'Content-Length': message.length,
+      'Content-Type': 'text/plain',
+			'x-amz-acl': 'private'
+  });
+  req.on('response', function(res){
+    if (200 == res.statusCode) {
+      console.log('saved to %s', req.url);
+    }
+  });
+  req.end(message.toString());
+}
+
+//Setup Socket.IO
+var io = io.listen(app);
+
+console.log("NO CONNECTION");
+
+var currentRoom = '';
+var currentImage = '';
+var roomNumber = 0;
+
+io.sockets.on('connection', function(socket){
+	socket.on('login', function () {
+		console.log('ROOMNUMBER: ' + roomNumber);
+		if (io.sockets.clients().length % 2 == 0) { // even: get in last room and increment roomNumber
+			knoxClient.get('images').on('response', function(res){
+			  console.log(res.statusCode);
+			  console.log(res.headers);
+			  res.setEncoding('utf8');
+			  res.on('data', function(chunk){
+					socket.emit('receive', 'images', chunk.split('\n')[roomNumber]);
+					knoxClient.get('chatrooms').on('response', function(res){
+					  console.log(res.statusCode);
+					  console.log(res.headers);
+					  res.setEncoding('utf8');
+					  res.on('data', function(chunk){
+							socket.emit('receive', 'chatrooms', chunk.split('\n')[roomNumber++]);
+					  });
+					}).end();
+			  });
+			}).end();
+		}
+		else { // odd: new room
+			knoxClient.get('images').on('response', function(res){
+			  console.log(res.statusCode);
+			  console.log(res.headers);
+			  res.setEncoding('utf8');
+			  res.on('data', function(chunk){
+					socket.emit('receive', 'images', chunk.split('\n')[roomNumber]);
+					knoxClient.get('chatrooms').on('response', function(res){
+					  console.log(res.statusCode);
+					  console.log(res.headers);
+					  res.setEncoding('utf8');
+					  res.on('data', function(chunk){
+							socket.emit('receive', 'chatrooms', chunk.split('\n')[roomNumber]);
+					  });
+					}).end();
+			  });
+			}).end();
+		}
+	});
+
+	socket.on('ask', function(type, callback) {
+		knoxClient.get(type).on('response', function(res){
+		  console.log(res.statusCode);
+		  console.log(res.headers);
+		  res.setEncoding('utf8');
+		  res.on('data', function(chunk){
+				socket.emit('receive', type, chunk);
+		  });
+		}).end();
+	});
+	
+	socket.on('save', function(type, data, callback) {
+		console.log("Saving data: " + data);
+		callback(writeJSON(data, type));
+	});
+	
+	socket.on('disconnect', function(){
+		if (io.sockets.clients().length % 2 == 0 && io.sockets.clients().length != 0) { // even: someone left a room but there's still another person there
+			--roomNumber;
+		}
+	});
+});
+
+
+///////////////////////////////////////////
+//              Routes                   //
+///////////////////////////////////////////
+
+/////// ADD ALL YOUR ROUTES HERE  /////////
+
+app.get('/', function(req,res){
+	res.render('index.ejs', {
+		locals : { 
+			header: '',
+			footer: '',
+			title : 'mturk Chatrooms',
+			description: 'Page Description',
+			author: 'Alexandre Testu',
+			analyticssiteid: 'XXXXXXX' 
+		}
+	});
+});
+
+app.get('/admin', function(req,res){
+	res.render('admin.ejs', {
+		locals : { 
+			header: '',
+			footer: '',
+			title : 'Edit chatrooms info',
+			description: 'Page Description',
+			author: 'Alexandre Testu',
+			analyticssiteid: 'XXXXXXX' 
+		}
+	});
+});
+
+//A Route for Creating a 500 Error (Useful to keep around)
+app.get('/500', function(req, res){
+	throw new Error('This is a 500 Error');
+});
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('/*', function(req, res){
+	throw new NotFound;
+});
+
+function NotFound(msg){
+	this.name = 'NotFound';
+	Error.call(this, msg);
+	Error.captureStackTrace(this, arguments.callee);
+}
---
+++ static/js/script.js
@@ -0,0 +1,40 @@
+/* Author: Alexandre Testu
+*/
+
+$(document).ready(function() {
+	var socket = io.connect('http://0.0.0.0', {'try multiple transports': true, 'transports': ['flashsocket', 'htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling']});
+	// var socket = io.connect('http://mturk-chatrooms.herokuapp.com', {'try multiple transports': true, 'transports': ['flashsocket', 'htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling']});
+
+	function admin () {
+		return ($('textarea').length != 0);
+	}
+
+	if (admin ()) {
+		socket.emit('ask', 'images');
+		socket.emit('ask', 'chatrooms');
+	}
+	else {
+		socket.emit('login');
+	}
+	
+	socket.on('receive', function (type, data) {
+		if (type == 'images') {
+			admin () ? $('#images').text(data) : $('img').attr('src', data);
+		}
+		else {
+			admin () ? $('#chatrooms').text(data) : $('#iframe').append(data);
+		}
+	});
+
+	$('#send').live('click', function () {
+		socket.emit('save', 'images', $('#images').val(), function (response) {
+			socket.emit('save', 'chatrooms', $('#chatrooms').val(), function (response) {			
+				console.log(response);
+				window.location.replace('/');
+			})
+		})
+	});
+
+	socket.on('connect', function() {
+	});
+});
